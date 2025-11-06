0719531834b8cea1bf086cd1afbb60c9947771fd
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-11-06 11:43:10 +0100

    adds the code for the jwt provider

---
+++ src/Adria.Application/Authentication/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Adria.Domain.Users;
+
+namespace Adria.Application.Authentication;
+
+public interface IJwtProvider
+{
+    string GenerateToken(User user);
+}
---
+++ src/Adria.Infrastructure/JwtProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Adria.Application.Authentication;
+using Adria.Domain.Users;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Adria.Infrastructure;
+
+public class JwtProvider : IJwtProvider
+{
+    public string GenerateToken(User user)
+    {
+        List<Claim> claims =
+        [
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()), // Subject is standardized for unique identifiers
+            new Claim(JwtRegisteredClaimNames.PreferredUsername, user.Username)
+        ];
+        
+        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.Secret));
+        SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        
+        var token = new JwtSecurityToken(
+            issuer: _config.Issuer,
+            audience: _config.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddDays(_config.ExpireDays),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
