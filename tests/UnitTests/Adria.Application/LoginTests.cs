221215ae923a20c2726a13bdf5d70faed376d21d
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-11-06 15:38:43 +0100

    adds tests for generating a jwt

---
+++ tests/UnitTests/Adria.Application/LoginTests.cs
@@ -0,0 +1,36 @@
+﻿using Adria.Application.Authentication;
+using Adria.Application.Contracts;
+using Adria.Application.Users;
+using Adria.Domain.Shared.Exceptions;
+using Adria.Domain.Users;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using UnitTests.Mocks;
+
+namespace UnitTests.Adria.Application;
+
+public class LoginTests
+{
+    [Fact]
+    public async Task LoginWithNonExistingUserThrows()
+    {
+        ILogger<Login> logger = new NullLogger<Login>();
+        IJwtProvider jwtProvider = new MockJwtProvider();
+        IUserRepository repository = new MockAdoUserRepository();
+        Login useCase = new(repository, logger, jwtProvider);
+
+        await Assert.ThrowsAsync<ElementNotFoundException>(() => useCase.Execute(Guid.NewGuid()));
+    }
+    
+    [Fact]
+    public async Task LoginWithExistingUserReturnsJwt()
+    {
+        ILogger<Login> logger = new NullLogger<Login>();
+        IJwtProvider jwtProvider = new MockJwtProvider();
+        IUserRepository repository = new MockAdoUserRepository();
+        Login useCase = new(repository, logger, jwtProvider);
+        User user = new("DitIsEenUsername");
+        await repository.Save(user);
+        Assert.NotNull(useCase.Execute(user.Id));
+    }
+}
---
+++ tests/UnitTests/Mocks/MockJwtProvider.cs
@@ -0,0 +1,12 @@
+﻿using Adria.Application.Authentication;
+using Adria.Domain.Users;
+
+namespace UnitTests.Mocks;
+
+public class MockJwtProvider : IJwtProvider
+{
+    public string GenerateToken(User user)
+    {
+        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1NTBlODQwMC1lMjliLTQxZDQtYTcxNi00NDY2NTU0NDAwMDAiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJBbGljZVNtaXRoIiwiZXhwIjoxNzYzMDQyNTM3LCJpc3MiOiJWaXJ0RWFydGggc2VydmVyIiwiYXVkIjoiVmlydEVhcnRoIHBsYXllciJ9.BjgPP2MD37ndrV6agF4mje3U1I0OZhG8J0vIyhmxxuQ";
+    }
+}
