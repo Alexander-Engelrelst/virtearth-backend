a08238bf243aab95f3e3b4da6953265501731fca
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-11-06 15:02:31 +0100

    fixes code to only return the jwt

--- src/Adria.Infrastructure/WebApi/Controllers/LoginController.cs
+++ src/Adria.Infrastructure/WebApi/Controllers/LoginController.cs
@@ -1,25 +1,32 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using Adria.Application.Contracts;
 using Adria.Application.Users;
+using Adria.Domain.Shared.Exceptions;
+using Adria.Infrastructure.Persistence.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Adria.Infrastructure.WebApi.Controllers.Responses;
+namespace Adria.Infrastructure.WebApi.Controllers;
 
 public class LoginController
 {
-    public static async Task<string> Invoke(
+    public static async Task<Results<Ok<string>, NotFound<string>, ProblemHttpResult>> Invoke(
         [FromRoute] Guid id,
         [FromServices] IUseCase<Guid, Task<string>> login
     )
     {
         try
         {
-            return await login.Execute(id);
+            return TypedResults.Ok(await login.Execute(id));
         }
-        catch (Exception e)
+        catch (ElementNotFoundException)
         {
-            Console.WriteLine(e);
-            throw;
+            return TypedResults.NotFound($"User with id {id} not found");
+        }
+        catch (VirtEarthDatabaseException)
+        {
+            return TypedResults.Problem("an unexpected error occured");
         }
     }
 }
--- src/Adria.Infrastructure/WebApi/Routes.cs
+++ src/Adria.Infrastructure/WebApi/Routes.cs
@@ -1,5 +1,4 @@
 using Adria.Infrastructure.WebApi.Controllers;
-using Adria.Infrastructure.WebApi.Controllers.Responses;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
