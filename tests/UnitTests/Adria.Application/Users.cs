4e05281dd53242c7233ace3550cbb8b6ad34b924
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-11-06 09:19:49 +0100

    adds the mockuserrepository

---
+++ tests/UnitTests/Adria.Application/Users.cs
@@ -0,0 +1,29 @@
+﻿using Adria.Application.Users;
+using Adria.Domain.Shared.Exceptions;
+using Microsoft.Extensions.Logging.Abstractions;
+using UnitTests.Mocks;
+
+namespace UnitTests.Adria.Application;
+
+public class Users
+{
+    [Fact]
+    public async Task ExecuteUsernameSearchWithExistingUserThrows()
+    {
+        var username = "jeffken";
+        var MockQuery = new MockUserExistsQuery();
+        var useCase = new CheckUsernameInUse(MockQuery, new NullLogger<CheckUsernameInUse>());
+        var input = new CheckUsernameInUseInput(username);
+        await Assert.ThrowsAsync<UsernameAlreadyExistsException>(() => useCase.Execute(input));
+    }
+    
+    [Fact]
+    public async Task ExecuteUsernameSearchWithNonExistingUserReturnsFalse()
+    {
+        var username = "bobke";
+        var MockQuery = new MockUserExistsQuery();
+        var useCase = new CheckUsernameInUse(MockQuery, new NullLogger<CheckUsernameInUse>());
+        var input = new CheckUsernameInUseInput(username);
+        Assert.False(await useCase.Execute(input));
+    }
+}
