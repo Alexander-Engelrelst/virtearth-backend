951958a7993f755c0eec0c7996f988f82153d4ff
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-11-06 13:12:17 +0100

    adds the config for the jwt

--- src/Adria.Infrastructure/Authentication/JwtProvider.cs
+++ src/Adria.Infrastructure/Authentication/JwtProvider.cs
@@ -10,26 +10,34 @@ namespace Adria.Infrastructure;
 
 public class JwtProvider : IJwtProvider
 {
+    private static readonly JwtConfiguration _jwtConfiguration = new JwtConfiguration();
     public string GenerateToken(User user)
     {
-        /*List<Claim> claims =
+        List<Claim> claims =
         [
             new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()), // Subject is standardized for unique identifiers
             new Claim(JwtRegisteredClaimNames.PreferredUsername, user.Username)
         ];
         
-        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.Secret));
+        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfiguration.Secret));
         SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
         
         var token = new JwtSecurityToken(
-            issuer: _config.Issuer,
-            audience: _config.Audience,
+            issuer: _jwtConfiguration.Issuer,
+            audience: _jwtConfiguration.Audience,
             claims: claims,
-            expires: DateTime.UtcNow.AddDays(_config.ExpireDays),
+            expires: DateTime.UtcNow.AddDays(_jwtConfiguration.ExpireDays),
             signingCredentials: creds
         );
 
-        return new JwtSecurityTokenHandler().WriteToken(token);*/
-        return "";
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
+}
+
+public record JwtConfiguration
+{
+    public string Secret { get; set; } = "L7dLr6B6K9M+f0Ogbbuv9y8RnWUVVYqJ7Zn1jZy4WGi8sVtzjQw1v5XvT4Qy2x+O9U9JvUdxW1BvJQnHVpEtDw==";
+    public string Issuer { get; set; } = "VirtEarth server";
+    public string Audience { get; set; } = "VirtEarth player";
+    public int ExpireDays { get; set; } = 7;
 }
---
+++ src/Adria.Main/Modules/Authentication/Module.cs
@@ -0,0 +1,14 @@
+﻿using Adria.Application.Authentication;
+using Adria.Infrastructure;
+
+namespace Adria.Main.Modules.Authentication;
+
+public static class Module
+{
+    private static IServiceCollection AddAdoServices(
+        this IServiceCollection services
+    )
+    {
+        return services.AddScoped<IJwtProvider, JwtProvider>();
+    }
+}
