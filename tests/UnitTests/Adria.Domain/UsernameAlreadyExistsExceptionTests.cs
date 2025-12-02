5c3feb2892277518f503531e104ced1600e9e157
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-12-02 18:08:46 +0100

    adds the tests for usernamealreadyexistsexception

--- tests/UnitTests/Adria.Domain/UsernameAlreadyExistsExceptionTests.cs
+++ tests/UnitTests/Adria.Domain/UsernameAlreadyExistsExceptionTests.cs
@@ -1,6 +1,35 @@
-﻿namespace UnitTests.Adria.Domain;
+﻿using Adria.Domain.Shared.Exceptions;
+
+namespace UnitTests.Adria.Domain;
 
 public class UsernameAlreadyExistsExceptionTests
 {
-    // TODO
+    [Fact]
+    public void Constructor_WithMessage_CreatesException()
+    {
+        // Arrange
+        var username = "jeffrey";
+        
+        // Act
+        var exception = new UsernameAlreadyExistsException(username);
+
+        // Assert
+        Assert.Equal($"username {username} already in use.", exception.Message);
+        Assert.Null(exception.InnerException);
+    }
+
+    [Fact]
+    public void Constructor_WithMessageAndInnerException_CreatesException()
+    {
+        // Arrange
+        var username = "jeffrey";
+        var innerException = new Exception("Inner exception");
+        
+        // Act
+        var exception = new UsernameAlreadyExistsException(username, innerException);
+        
+        // Assert
+        Assert.Equal($"username {username} already in use.", exception.Message);
+        Assert.Equal(innerException, exception.InnerException);
+    }
 }
