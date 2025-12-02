7d71b49e3bb3a19a172388866b6af7e1439d7978
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-12-02 18:10:31 +0100

    adds the tests for the mazegenerationexception

--- tests/UnitTests/Adria.Domain/MazeGenerationExceptionTests.cs
+++ tests/UnitTests/Adria.Domain/MazeGenerationExceptionTests.cs
@@ -1,6 +1,36 @@
-﻿namespace UnitTests.Adria.Domain;
+﻿using Adria.Domain.Shared.Exceptions;
+
+namespace UnitTests.Adria.Domain;
 
 public class MazeGenerationExceptionTests
 {
-    // TODO
+    
+    [Fact]
+    public void Constructor_WithMessage_CreatesException()
+    {
+        // Arrange
+        var message = "this is a very useful message";
+        
+        // Act
+        var exception = new MazeGenerationException(message);
+
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Null(exception.InnerException);
+    }
+
+    [Fact]
+    public void Constructor_WithMessageAndInnerException_CreatesException()
+    {
+        // Arrange
+        var message = "this is a very useful message";
+        var innerException = new Exception("Inner exception");
+        
+        // Act
+        var exception = new MazeGenerationException(message, innerException);
+        
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Equal(innerException, exception.InnerException);
+    }
 }
