8a8567450a42a1b2abc326c8ff491e80fee6f0f6
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-12-08 20:47:37 +0100

    adds an exception for when a player is already playing a game

---
+++ tests/UnitTests/Adria.Domain/PlayerAlreadyPlayingExceptionTests.cs
@@ -0,0 +1,35 @@
+﻿using Adria.Domain.Shared.Exceptions;
+
+namespace UnitTests.Adria.Domain;
+
+public class PlayerAlreadyPlayingExceptionTests
+{
+    [Fact]
+    public void Constructor_WithMessage_CreatesException()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        
+        // Act
+        var exception = new PlayerAlreadyPlayingException(id);
+
+        // Assert
+        Assert.Equal($"username {id} already playing a game.",exception.Message);
+        Assert.Null(exception.InnerException);
+    }
+
+    [Fact]
+    public void Constructor_WithMessageAndInnerException_CreatesException()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var innerException = new Exception("Inner exception");
+        
+        // Act
+        var exception = new PlayerAlreadyPlayingException(id, innerException);
+        
+        // Assert
+        Assert.Equal($"username {id} already playing a game.",exception.Message);
+        Assert.Equal(innerException, exception.InnerException);
+    }
+}
