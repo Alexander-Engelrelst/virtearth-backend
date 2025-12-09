0410783882617f0aac496f5eed76f516cb51c6cb
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-12-09 17:44:38 +0100

    adds test for GameIdMisMatchException

--- src/Adria.Domain/Shared/Exceptions/GameIdMismatchException.cs
+++ src/Adria.Domain/Shared/Exceptions/GameIdMismatchException.cs
@@ -3,4 +3,4 @@
 namespace Adria.Domain.Shared.Exceptions;
 
 public class GameIdMismatchException(Guid userId, Exception? innerException = null) : Exception(
-    $"User {userId} is trying to access a game different to the one he's currently playign", innerException);
+    $"User {userId} is trying to access a game different to the one he's currently playing", innerException);
