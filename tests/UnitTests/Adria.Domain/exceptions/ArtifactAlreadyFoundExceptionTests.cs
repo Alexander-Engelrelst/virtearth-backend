e8b7149bdc61b08fa8e96d5eca0e086f5e7bd289
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-12-09 17:41:56 +0100

    adds test for the artifactalreadyfontexception

--- tests/UnitTests/Adria.Domain/exceptions/ArtifactAlreadyFoundExceptionTests.cs
+++ tests/UnitTests/Adria.Domain/exceptions/ArtifactAlreadyFoundExceptionTests.cs
@@ -2,22 +2,21 @@
 
 namespace UnitTests.Adria.Domain.exceptions;
 
-public class ArtifactNotFoundExceptionTests
+public class ArtifactAlreadyFoundExceptionTests
 {
     [Fact]
     public void Constructor_WithMessage_CreatesException()
     {
         // Arrange
         var userId = Guid.NewGuid();
-        var gameId = Guid.NewGuid();
         var artifactId = Guid.NewGuid();
         
         // Act
-        var exception = new ArtifactNotFoundException(userId, artifactId, gameId);
+        var exception = new ArtifactAlreadyFoundException(userId, artifactId);
 
         // Assert
         Assert.Equal(
-            $"User {userId} is trying to find a non-existing artifact {artifactId} in {gameId}", exception.Message
+            $"User {userId} already found artifact {artifactId}", exception.Message
         );
         Assert.Null(exception.InnerException);
     }
@@ -27,16 +26,16 @@ public class ArtifactNotFoundExceptionTests
     {
         // Arrange
         var innerException = new Exception("Inner exception");
+
         var userId = Guid.NewGuid();
-        var gameId = Guid.NewGuid();
         var artifactId = Guid.NewGuid();
         
         // Act
-        var exception = new ArtifactNotFoundException(userId, artifactId, gameId, innerException);
+        var exception = new ArtifactAlreadyFoundException(userId, artifactId, innerException);
 
         // Assert
         Assert.Equal(
-            $"User {userId} is trying to find a non-existing artifact {artifactId} in {gameId}", exception.Message
+            $"User {userId} already found artifact {artifactId}", exception.Message
         );
         Assert.Equal(innerException, exception.InnerException);
     }
