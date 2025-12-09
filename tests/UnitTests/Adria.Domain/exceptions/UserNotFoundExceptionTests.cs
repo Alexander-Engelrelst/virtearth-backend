6032bae6979c0e1f5e6e79f49df58c267051c8c9
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-12-09 18:27:37 +0100

    adds the test for usernotfoundexception

---
+++ tests/UnitTests/Adria.Domain/exceptions/UserNotFoundExceptionTests.cs
@@ -0,0 +1,36 @@
+﻿using Adria.Domain.Shared.Exceptions;
+
+namespace UnitTests.Adria.Domain.exceptions;
+
+public class UserNotFoundExceptionTests
+{
+    [Fact]
+    public void Constructor_WithMessage_CreatesException()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        
+        // Act
+        var exception = new UserNotFoundException(userId);
+
+        // Assert
+        Assert.Equal($"User with id {userId} not found", exception.Message);
+        Assert.Null(exception.InnerException);
+    }
+
+    [Fact]
+    public void Constructor_WithMessageAndInnerException_CreatesException()
+    {
+        // Arrange
+        var innerException = new Exception("Inner exception");
+        var userId = Guid.NewGuid();
+        
+        // Act
+        var exception = new UserNotFoundException(userId, innerException);
+
+        // Assert
+        Assert.Equal($"User with id {userId} not found", exception.Message);
+
+        Assert.Equal(innerException, exception.InnerException);
+    }
+}
