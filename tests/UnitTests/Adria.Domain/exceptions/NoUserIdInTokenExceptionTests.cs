f4f52de154cc5121ecb52b4b23a614d77aa9a54c
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-12-09 17:47:07 +0100

    adds a test for the noUserIdInTokenException

---
+++ tests/UnitTests/Adria.Domain/exceptions/NoUserIdInTokenExceptionTests.cs
@@ -0,0 +1,35 @@
+﻿using Adria.Domain.Shared.Exceptions;
+
+namespace UnitTests.Adria.Domain.exceptions;
+
+public class NoUserIdInTokenExceptionTests
+{
+    [Fact]
+    public void Constructor_WithMessage_CreatesException()
+    {
+        // Act
+        var exception = new NoUserIdInTokenException();
+
+        // Assert
+        Assert.Equal(
+            "The is no user id in the JWT token", exception.Message
+        );
+        Assert.Null(exception.InnerException);
+    }
+
+    [Fact]
+    public void Constructor_WithMessageAndInnerException_CreatesException()
+    {
+        // Arrange
+        var innerException = new Exception("Inner exception");
+        
+        // Act
+        var exception = new NoUserIdInTokenException(innerException);
+
+        // Assert
+        Assert.Equal(
+            "The is no user id in the JWT token", exception.Message
+        );
+        Assert.Equal(innerException, exception.InnerException);
+    }
+}
