2d084b2d1fab47f144abd86e245718c7a1006bdd
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-12-15 09:20:28 +0100

    adds the test for the duplicatePrimaryKeyException

---
+++ tests/UnitTests/Adria.Infrastructure/DuplicatePrimaryKeyExceptionTests.cs
@@ -0,0 +1,35 @@
+﻿using Adria.Infrastructure.Persistence.Shared;
+
+namespace UnitTests.Adria.Infrastructure;
+
+public class DuplicatePrimaryKeyExceptionTests
+{
+    [Fact]
+    public void Constructor_WithMessage_CreatesException()
+    {
+        // Arrange
+        var message = "this is a very useful message";
+        
+        // Act
+        var exception = new DuplicatePrimaryKeyException(message);
+
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Null(exception.InnerException);
+    }
+
+    [Fact]
+    public void Constructor_WithMessageAndInnerException_CreatesException()
+    {
+        // Arrange
+        var message = "this is a very useful message";
+        var innerException = new Exception("Inner exception");
+        
+        // Act
+        var exception = new DuplicatePrimaryKeyException(message, innerException);
+        
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Equal(innerException, exception.InnerException);
+    }
+}
